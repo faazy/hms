e625fa4b56ac10ec6eed96f836bce6ff965fba7b
FaAzI <faaziahamed@gmail.com>
2021-04-04 13:36:09 +0530

    update Readme. Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+
+        $faker = \Faker\Factory::create();
+
+        User::create([
+            'name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => 'admin@hms.com',
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
